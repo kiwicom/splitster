772c241ce712a3f865993d6b03fac3b6f6b7e6cf
Robin Cussol <me@robincussol.com>
2020-11-09 17:57:03 +0100

    feat: attempt at typing Splitster entities in TS

---
+++ src/records/config/index_WIP.ts
@@ -0,0 +1,48 @@
+import * as R from "ramda";
+import { SplitsterInitConfig, TestConfiguration, VariantConfiguration } from "../../..";
+
+export const defaultConfig: SplitsterInitConfig<{}, {}> = {
+  tests: {},
+  options: {
+    cookies: {
+      disabled: false,
+      expiration: 30, // TODO: check if it is alright
+      name: "splitster"
+    }
+  }
+};
+
+export const defaultTestConfig: TestConfiguration<{}, {"": VariantConfiguration}> = {
+  description: "",
+  userGroup: {},
+  userGroupExclude: {},
+  usage: 100,
+  defaultVariant: "",
+  variants: {},
+  disabled: false,
+  disabledReason: null,
+  version: 0
+};
+
+export const mapVariants = R.mapObjIndexed((variant, key) => {
+  if (R.is(Number, variant)) {
+    return {
+      value: key,
+      ratio: variant
+    };
+  }
+  return variant;
+});
+
+export const mergeTestConfig = R.compose(
+  test => R.assoc("variants", mapVariants(test.variants), test),
+  R.mergeDeepRight(defaultTestConfig)
+);
+
+export const mergeDefaultConfig = config => {
+  const merged = R.compose(
+    c => R.assoc("tests", R.map(mergeTestConfig, config.tests), c),
+    R.mergeDeepRight(defaultConfig)
+  )(config);
+  return merged;
+};
