799e1b691238a3d7d8c092a02b0a2aea282b78b0
Robin Cussol <me@robincussol.com>
2020-11-18 17:28:51 +0100

    chore: set up TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,17 @@ module.exports = {
   extends: ["@kiwicom"],
   rules: {
     "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "jest/prefer-expect-assertions": "OFF",
   },
   overrides: [
+    {
+      // TypeScript
+      files: ["*.ts", "*.tsx"],
+      extends: ["@kiwicom/eslint-config/ts"],
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
     {
       // Flow
       files: ["*.js", "*.jsx", "*.js.flow"],
