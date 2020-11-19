3da29e34caab175da2c2fe0d003d2551d523a81c
Robin Cussol <me@robincussol.com>
2020-11-19 14:48:24 +0100

    feat: add shared types
    I tried to make them as generic as possible for
    the type inference in VSCode to be nice and
    useful by deriving possible values from User and
    Variants.

---
+++ src/index.ts
@@ -0,0 +1,83 @@
+export type DisabledReason =
+  | "usage"
+  | "separate_test"
+  | "user_group"
+  | "user_group_exclude"
+  | "config"
+  | "deadline"
+  | "dev";
+
+// Tests
+export type StandardVariantConfiguration = {
+  value: string;
+  ratio: number;
+};
+export type ShortVariantConfiguration = number;
+export type VariantConfiguration = StandardVariantConfiguration | ShortVariantConfiguration;
+export type VariantId = string;
+export type Variants = Record<VariantId, VariantConfiguration>;
+export type StandardVariants = Record<VariantId, StandardVariantConfiguration>;
+
+export type User = Record<string, unknown>;
+
+export type TestId = string;
+export type TestConfiguration<U extends User = User, V extends Variants = Variants> = {
+  /**
+   * Short description - optional
+   */
+  description?: string;
+  /**
+   * Groups which user must satisfy - optional
+   */
+  userGroup?: UserGroup<U>;
+  /**
+   * Groups which user must satisfy - optional
+   */
+  userGroupExclude?: UserGroup<U>;
+  /**
+   *  Overall usage of test in % - optional
+   * Iif not specified 100 is used
+   */
+  usage?: number;
+
+  /**
+   *  If the test is disabled, it always returns the default variant
+   */
+  disabled?: boolean;
+  /**
+   *  If the test is disabled, it always returns the default variant
+   */
+  disabledReason?: DisabledReason | null;
+
+  /**
+   *  Default variant id
+   */
+  defaultVariant: keyof V | null;
+
+  /**
+   * Variants of the test specified by id.
+   */
+  variants: V;
+
+  /**
+   * Version of the test - optional
+   * Defaults to 0
+   */
+  version?: number;
+};
+
+export type Tests<U extends User = User, V extends Variants = Variants> = Record<
+  TestId,
+  TestConfiguration<U, V>
+>;
+
+// UserGroups
+export type UserGroupName = string;
+export type UserGroupRule<U extends User = User> =
+  | Record<keyof U, unknown>
+  | ((user: U) => boolean);
+export type UserGroup<U extends User = User> = UserGroupRule<U> | UserGroupRule<U>[];
+
+export type SplitsterInitConfig<U extends User = User, V extends Variants = Variants> = {
+  tests: Tests<U, V>;
+};
